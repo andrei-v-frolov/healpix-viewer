f3c5b78da17801b4c750fc95b70e84fc7949d0f7
Andrei Frolov <frolov@sfu.ca>
2022-11-12 13:36:48 -0800

    KVO observer for UserDefaults added.

--- HEALPix Viewer/macOS UI/HEALPix_ViewerApp.swift
+++ HEALPix Viewer/macOS UI/HEALPix_ViewerApp.swift
@@ -22,4 +22,31 @@ struct HEALPix_ViewerApp: App {
     init() {
         NSWindow.allowsAutomaticWindowTabbing = false
     }
+    
+    // application appearance
+    let appearanceObserver = AppStorageObserver(key: Appearance.appStorage) { old, new in
+        guard let raw = new as? String, let mode = Appearance(rawValue: raw) else { return }
+        
+        NSApp.appearance = mode.appearance
+    }
+}
+
+// observer for UserDefaults changes
+class AppStorageObserver: NSObject {
+    let key: String
+    private var onChange: (Any, Any) -> Void
+
+    init(key: String, onChange: @escaping (Any, Any) -> Void) {
+        self.key = key; self.onChange = onChange; super.init()
+        UserDefaults.standard.addObserver(self, forKeyPath: key, options: [.old, .new], context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        guard let change = change, object != nil, keyPath == key else { return }
+        onChange(change[.oldKey] as Any, change[.newKey] as Any)
+    }
+    
+    deinit {
+        UserDefaults.standard.removeObserver(self, forKeyPath: key, context: nil)
+    }
 }
