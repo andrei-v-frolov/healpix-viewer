bd7e512bc7f9814903346993f7778bfea20d6a14
Andrei Frolov <frolov@sfu.ca>
2022-11-15 08:20:41 -0800

    Open file dialog and HEALPix UTType added.

---
+++ HEALPix Viewer/macOS UI/FileInput.swift
@@ -0,0 +1,28 @@
+//
+//  FileInput.swift
+//  HEALPix Viewer
+//
+//  Created by Andrei Frolov on 2022-11-15.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+
+extension UTType {
+    static var healpix: UTType { UTType(exportedAs: "public.data.fits.healpix") }
+}
+
+func showOpenPanel() -> URL? {
+    let panel = NSOpenPanel()
+    
+    panel.canChooseFiles = true
+    panel.canChooseDirectories = false
+    panel.allowsMultipleSelection = false
+    panel.isExtensionHidden = false
+    
+    panel.allowedContentTypes = [UTType.healpix]
+    panel.allowsOtherFileTypes = true
+    
+    let response = panel.runModal()
+    return response == .OK && (panel.url?.isFileURL ?? false) ? panel.url : nil
+}
