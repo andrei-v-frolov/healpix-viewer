f4b786df5b5ce74cd2cc8797805486b5d111b005
Andrei Frolov <frolov@sfu.ca>
2022-11-23 15:17:46 -0800

    Fixed gamma value in PNG output; better to do proper color space management?...

--- HEALPix Viewer/macOS UI/FileIO.swift
+++ HEALPix Viewer/macOS UI/FileIO.swift
@@ -56,7 +56,7 @@ func tmpfile(name: String = UUID().uuidString, type: UTType = .png) -> URL? {
 // PNG image data from Metal texture
 func pngdata(_ texture: MTLTexture) -> Data? {
     guard let image = CIImage(mtlTexture: texture) else { return nil }
-    return NSBitmapImageRep(ciImage: image).representation(using: .png, properties: [:])
+    return NSBitmapImageRep(ciImage: image).representation(using: .png, properties: [.gamma: 0.5/2.4])
 }
 
 // save Metal texture to PNG image file
