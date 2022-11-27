f3ce448154c03dd60cf4c3191123325b2a5fdff6
Andrei Frolov <frolov@sfu.ca>
2022-11-27 15:27:57 -0800

    Metal shader updated to operate on 1D, 2D, & 3D arguments...

--- HEALPix Viewer/Metal/MetalShader.swift
+++ HEALPix Viewer/Metal/MetalShader.swift
@@ -13,6 +13,8 @@ protocol MetalShader {
     var name: String { get }
     
     // encode shader to command buffer specified
+    func encode(command: MTLCommandBuffer, buffers: [MTLBuffer])
+    func encode(command: MTLCommandBuffer, buffers: MTLBuffer...)
     func encode(command: MTLCommandBuffer, textures: [MTLTexture])
     func encode(command: MTLCommandBuffer, textures: MTLTexture...)
     func encode(command: MTLCommandBuffer, buffers: [MTLBuffer], textures: [MTLTexture], threadsPerGrid: MTLSize?)
@@ -33,6 +35,15 @@ extension MetalShader {
         encode(command: command, buffers: buffers, textures: textures, threadsPerGrid: threadsPerGrid)
     }
     
+    // convenience encoders which operate on buffers only
+    func encode(command: MTLCommandBuffer, buffers: [MTLBuffer]) {
+        encode(command: command, buffers: buffers, textures: [], threadsPerGrid: nil)
+    }
+    
+    func encode(command: MTLCommandBuffer, buffers literal: MTLBuffer...) {
+        encode(command: command, buffers: literal, textures: [], threadsPerGrid: nil)
+    }
+    
     // convenience encoders which operate on textures only
     func encode(command: MTLCommandBuffer, textures: [MTLTexture]) {
         encode(command: command, buffers: [], textures: textures, threadsPerGrid: nil)
@@ -47,7 +58,9 @@ extension MetalShader {
 struct MetalKernel: MetalShader {
     let name: String
     let state: MTLComputePipelineState
-    var threadsPerGroup: MTLSize
+    var threadsPerGroup1D: MTLSize
+    var threadsPerGroup2D: MTLSize
+    var threadsPerGroup3D: MTLSize { threadsPerGroup2D }
     
     // main initializer
     init(kernel: String, device provided: MTLDevice? = nil) {
@@ -65,7 +78,8 @@ struct MetalKernel: MetalShader {
         
         // default thread group configuration
         let threads = state.maxTotalThreadsPerThreadgroup, width = state.threadExecutionWidth
-        threadsPerGroup = MTLSize(width: width, height: threads/width, depth: 1)
+        threadsPerGroup1D = MTLSize(width: threads, height: 1, depth: 1)
+        threadsPerGroup2D = MTLSize(width: width, height: threads/width, depth: 1)
     }
     
     // dispatch compute encoder threads for execution
@@ -74,11 +88,25 @@ struct MetalKernel: MetalShader {
         // encoder.dispatchThreads(threadsPerGrid, threadsPerThreadgroup: threadsPerGroup)
         
         // dispatch thread groups to cover entire grid
-        let w = (threadsPerGrid.width + threadsPerGroup.width - 1)/threadsPerGroup.width
-        let h = (threadsPerGrid.height + threadsPerGroup.height - 1)/threadsPerGroup.height
-        let d = (threadsPerGrid.depth + threadsPerGroup.depth - 1)/threadsPerGroup.depth
-        let groupsPerGrid = MTLSize(width: w, height: h, depth: d)
-        encoder.dispatchThreadgroups(groupsPerGrid, threadsPerThreadgroup: threadsPerGroup)
+        if (threadsPerGrid.depth > 1) {
+            let w = (threadsPerGrid.width + threadsPerGroup3D.width - 1)/threadsPerGroup3D.width
+            let h = (threadsPerGrid.height + threadsPerGroup3D.height - 1)/threadsPerGroup3D.height
+            let d = (threadsPerGrid.depth + threadsPerGroup3D.depth - 1)/threadsPerGroup3D.depth
+            
+            let groupsPerGrid = MTLSize(width: w, height: h, depth: d)
+            encoder.dispatchThreadgroups(groupsPerGrid, threadsPerThreadgroup: threadsPerGroup3D)
+        } else if (threadsPerGrid.height > 1) {
+            let w = (threadsPerGrid.width + threadsPerGroup2D.width - 1)/threadsPerGroup2D.width
+            let h = (threadsPerGrid.height + threadsPerGroup2D.height - 1)/threadsPerGroup2D.height
+            
+            let groupsPerGrid = MTLSize(width: w, height: h, depth: 1)
+            encoder.dispatchThreadgroups(groupsPerGrid, threadsPerThreadgroup: threadsPerGroup2D)
+        } else {
+            let w = (threadsPerGrid.width + threadsPerGroup1D.width - 1)/threadsPerGroup1D.width
+            
+            let groupsPerGrid = MTLSize(width: w, height: 1, depth: 1)
+            encoder.dispatchThreadgroups(groupsPerGrid, threadsPerThreadgroup: threadsPerGroup1D)
+        }
     }
     
     // encode kernel into provided command buffer, with arguments passed as dictionaries
@@ -98,7 +126,7 @@ struct MetalKernel: MetalShader {
     // encode kernel into provided command buffer, with arguments passed as arrays
     func encode(command: MTLCommandBuffer, buffers: [MTLBuffer], textures: [MTLTexture], threadsPerGrid provided: MTLSize? = nil) {
         // initialize compute command encoder for kernel execution
-        guard let threadsPerGrid = provided ?? textures.last?.size else { return }
+        guard let threadsPerGrid = provided ?? textures.last?.size ?? buffers.first?.size else { return }
         guard let encoder = command.makeComputeCommandEncoder() else { return }
         
         // encode kernel for execution and set its arguments
@@ -109,6 +137,11 @@ struct MetalKernel: MetalShader {
         // dispatch compute pass and end command encoding
         dispatch(encoder, threadsPerGrid: threadsPerGrid); encoder.endEncoding()
     }
+}
+
+// MARK: convenience extensions to MTLBuffer
+extension MTLBuffer {
+    var size: MTLSize { return MTLSize(width: length/MemoryLayout<Float>.size, height: 1, depth: 1) }
 }
 
 // MARK: convenience extensions to MTLTexture
