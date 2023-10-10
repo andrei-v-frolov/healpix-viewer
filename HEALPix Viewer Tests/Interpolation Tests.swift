66616af526a0b36b6b59d925ba90c1aef52e85c2
Andrei Frolov <frolov@sfu.ca>
2023-10-09 20:44:51 -0700

    Interpolation code optimized and tested.

--- HEALPix Viewer Tests/Interpolation Tests.swift
+++ HEALPix Viewer Tests/Interpolation Tests.swift
@@ -18,37 +18,69 @@ final class Interpolation_Tests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func test_spline_3() throws {
-        let epsilon = 0.5/(pow(Double(pts),2))
-        
-        for _ in 0..<tries {
-            let x1 = Double.random(in: 0...1), x2 = Double.random(in: 0...1), x3 = Double.random(in: 0...1)
-            
-            let x = Array(0..<pts).map { Double($0)/Double(pts-1) }
-            let y = x.map { x in SIMD4<Double>((x-x1)*(x-x2)*(x-x3)) }
-            let spline = CubicSpline(x: x, y: y)
-            
-            for i in 0..<8*pts {
-                let x = Double(i)/Double(8*pts-1)
-                XCTAssertEqual(spline[x][0], (x-x1)*(x-x2)*(x-x3), accuracy: epsilon)
-            }
-        }
-    }
-    
-    func test_spline_4() throws {
-        let epsilon = 0.5/(pow(Double(pts),2))
+    func test_linear_4() throws {
+        let epsilon = 1.5/(pow(Double(pts),2)), n = 8*pts
         
         for _ in 0..<tries {
             let x1 = Double.random(in: 0...1), x2 = Double.random(in: 0...1), x3 = Double.random(in: 0...1), x4 = Double.random(in: 0...1)
             
             let x = Array(0..<pts).map { Double($0)/Double(pts-1) }
-            let y = x.map { x in SIMD4<Double>((x-x1)*(x-x2)*(x-x3)*(x-x4)) }
-            let spline = CubicSpline(x: x, y: y)
+            let y = x.map { x in SIMD4<Double>((x-x1)*(x-x2)*(x-x3)*(x-x4), (x-x1)*(x-x2)*(x-x3), (x-x1)*(x-x2), (x-x1)) }
+            let spline = CubicSpline(x: x, y: y), lut = spline.linear(lut: n)
             
-            for i in 0..<8*pts {
-                let x = Double(i)/Double(8*pts-1)
-                XCTAssertEqual(spline[x][0], (x-x1)*(x-x2)*(x-x3)*(x-x4), accuracy: epsilon)
+            for i in 0..<n {
+                let x = Double(i)/Double(n-1)
+                XCTAssertEqual(spline.linear(x)[0], (x-x1)*(x-x2)*(x-x3)*(x-x4), accuracy: epsilon)
+                XCTAssertEqual(spline.linear(x)[1], (x-x1)*(x-x2)*(x-x3), accuracy: epsilon)
+                XCTAssertEqual(spline.linear(x)[2], (x-x1)*(x-x2), accuracy: epsilon)
+                XCTAssertEqual(spline.linear(x)[3], (x-x1), accuracy: epsilon)
+                XCTAssertEqual(lut[i], spline.linear(x))
             }
         }
+    }
+    
+    func test_spline_4() throws {
+        let epsilon = 0.5/(pow(Double(pts),2)), n = 8*pts
+        
+        for _ in 0..<tries {
+            let x1 = Double.random(in: 0...1), x2 = Double.random(in: 0...1), x3 = Double.random(in: 0...1), x4 = Double.random(in: 0...1)
+            
+            let x = Array(0..<pts).map { Double($0)/Double(pts-1) }
+            let y = x.map { x in SIMD4<Double>((x-x1)*(x-x2)*(x-x3)*(x-x4), (x-x1)*(x-x2)*(x-x3), (x-x1)*(x-x2), (x-x1)) }
+            let spline = CubicSpline(x: x, y: y), lut = spline.cubic(lut: n)
+            
+            for i in 0..<n {
+                let x = Double(i)/Double(n-1)
+                XCTAssertEqual(spline[x][0], (x-x1)*(x-x2)*(x-x3)*(x-x4), accuracy: epsilon)
+                XCTAssertEqual(spline[x][1], (x-x1)*(x-x2)*(x-x3), accuracy: epsilon)
+                XCTAssertEqual(spline[x][2], (x-x1)*(x-x2), accuracy: epsilon)
+                XCTAssertEqual(spline[x][3], (x-x1), accuracy: epsilon)
+                XCTAssertEqual(lut[i], spline[x])
+            }
+        }
+    }
+    
+    func test_init_double4() throws {
+        let x1 = Double.random(in: 0...1), x2 = Double.random(in: 0...1), x3 = Double.random(in: 0...1), x4 = Double.random(in: 0...1)
+        
+        let pts = 2<<13, x = Array(0..<pts).map { Double($0)/Double(pts-1) }
+        let y = x.map { x in SIMD4<Double>((x-x1)*(x-x2)*(x-x3)*(x-x4), (x-x1)*(x-x2)*(x-x3), (x-x1)*(x-x2), (x-x1)) }
+        self.measure { let spline = CubicSpline(x: x, y: y) }
+    }
+    
+    func test_lut_double4() throws {
+        let x1 = Double.random(in: 0...1), x2 = Double.random(in: 0...1), x3 = Double.random(in: 0...1), x4 = Double.random(in: 0...1)
+        
+        let pts = 16, x = Array(0..<pts).map { Double($0)/Double(pts-1) }
+        let y = x.map { x in SIMD4<Double>((x-x1)*(x-x2)*(x-x3)*(x-x4), (x-x1)*(x-x2)*(x-x3), (x-x1)*(x-x2), (x-x1)) }
+        self.measure { let spline = CubicSpline(x: x, y: y); let _ = spline.cubic(lut: 2<<15) }
+    }
+    
+    func test_lookup_double4() throws {
+        let x1 = Double.random(in: 0...1), x2 = Double.random(in: 0...1), x3 = Double.random(in: 0...1), x4 = Double.random(in: 0...1)
+        
+        let pts = 16, x = Array(0..<pts).map { Double($0)/Double(pts-1) }
+        let y = x.map { x in SIMD4<Double>((x-x1)*(x-x2)*(x-x3)*(x-x4), (x-x1)*(x-x2)*(x-x3), (x-x1)*(x-x2), (x-x1)) }
+        self.measure { let spline = CubicSpline(x: x, y: y); for _ in 0..<2<<15 { let _ = spline[Double.random(in: 0...1)] } }
     }
 }
--- HEALPix Viewer/Colormaps/Interpolation.swift
+++ HEALPix Viewer/Colormaps/Interpolation.swift
@@ -8,7 +8,7 @@
 import Foundation
 import simd
 
-// cubic spline interpolator [adopted from https://github.com/gscalzo/SwiftCubicSpline]
+// cubic spline interpolator [heavily optimized from https://github.com/gscalzo/SwiftCubicSpline]
 struct CubicSpline<Value> where Value: SIMD, Value.Scalar: FloatingPoint {
     private var x: [Value.Scalar]
     private var a: [Value]
@@ -16,11 +16,59 @@ struct CubicSpline<Value> where Value: SIMD, Value.Scalar: FloatingPoint {
     private var c: [Value]
     private var d: [Value]
     
-    // lookup interpolated value
-    subscript(_ value: Value.Scalar) -> Value {
-        let i = x.lastIndex(where: { $0 <= value }) ?? x.startIndex, dx = value - x[i]
+    // bisection search (only useful for large arrays)
+    func find(_ value: Value.Scalar) -> Int {
+        guard x.count > 16 else { return x.lastIndex(where: { $0 <= value }) ?? x.startIndex }
+        guard let u = x.first, value > u else { return x.startIndex }
+        guard let u = x.last,  value < u else { return x.endIndex-1 }
+        
+        var low = x.startIndex, high = x.endIndex-1
+        while low+1 < high { let mid = (low+high)/2; if value < x[mid] { high = mid } else { low = mid} }
+        
+        return low
+    }
+    
+    // linear interpolated value
+    func linear(_ value: Value.Scalar) -> Value {
+        let i = min(find(value),max(x.startIndex,x.endIndex-2))
+        let q = (value - x[i])/(x[i+1] - x[i])
+        return (1-q)*a[i] + q*a[i+1]
+    }
+    
+    // linear interpolated LUT
+    func linear(lut n: Int) -> [Value] {
+        var y = [Value](repeating: .zero, count: n), k = x.startIndex
+        
+        for i in 0..<n {
+            let value = Value.Scalar(i)/Value.Scalar(n-1)
+            while k < x.endIndex-2, x[k+1] <= value { k += 1 }
+            let q = (value - x[k])/(x[k+1] - x[k]); y[i] = (1-q)*a[k] + q*a[k+1]
+        }
+        
+        return y
+    }
+    
+    // cubic interpolated value
+    func cubic(_ value: Value.Scalar) -> Value {
+        let i = find(value), dx = value - x[i]
         return a[i] + dx*(b[i] + dx*(c[i] + dx*d[i]))
     }
+    
+    // cubic interpolated LUT
+    func cubic(lut n: Int) -> [Value] {
+        var y = [Value](repeating: .zero, count: n), k = x.startIndex
+        
+        for i in 0..<n {
+            let value = Value.Scalar(i)/Value.Scalar(n-1)
+            while k < x.endIndex-1, x[k+1] <= value { k += 1 }
+            let dx = value - x[k]; y[i] = a[k] + dx*(b[k] + dx*(c[k] + dx*d[k]))
+        }
+        
+        return y
+    }
+    
+    // lookup interpolated value
+    subscript(_ value: Value.Scalar) -> Value { cubic(value) }
     
     // initialize spline LUT
     init(x: [Value.Scalar], y: [Value]) {
